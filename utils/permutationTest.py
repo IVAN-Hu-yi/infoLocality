21d147e532a4527729479f5e5dee8c6438f6d77e
IVAN-Hu-yi <ivann1329@outlook.com>
2025-11-27 16:11:28 +0100

    modify permutationTest

--- utils/permutationTest.py
+++ utils/permutationTest.py
@@ -4,14 +4,15 @@ def permutation_test(data1, data2, num_permutations=1000):
     observed_diff = np.abs(np.mean(data1) - np.mean(data2))
     combined = np.concatenate([data1, data2])
     count = 0
+    shuffDiffs = np.zeros(num_permutations)
 
-    for _ in range(num_permutations):
+    for i in range(num_permutations):
         np.random.shuffle(combined)
         new_data1 = combined[:len(data1)]
         new_data2 = combined[len(data1):]
         new_diff = np.abs(np.mean(new_data1) - np.mean(new_data2))
-        if abs(new_diff) >= abs(observed_diff):
-            count += 1
+        shuffDiffs[i] = new_diff
 
-    p_value = count / num_permutations
-    return p_value
+    p_value = np.sum(shuffDiffs >= observed_diff) / num_permutations
+
+    return p_value, shuffDiffs, observed_diff
